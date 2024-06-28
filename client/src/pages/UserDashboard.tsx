d6bb8911b1f91e18ae2fc1ad84daee309aae51ac
maithi-trandiep <thi.tdm04@gmail.com>
2024-06-28 21:27:09 +0200

    style tabslist

--- client/src/pages/UserDashboard.tsx
+++ client/src/pages/UserDashboard.tsx
@@ -15,58 +15,58 @@ const UserDashboard = () => {
 
     return (
         <>
-  
-                    <main className="p-6 space-y-6">
-                        <Tabs defaultValue='glycemie'>
-                            <TabsList className="flex space-x-4">
-                                <TabsTrigger value='glycemie'>Diabète</TabsTrigger>
-                                <TabsTrigger value='hypertension'>Hypertension</TabsTrigger>
-                                <TabsTrigger value='rappel'>Rappels</TabsTrigger>
-                            </TabsList>
-                            <TabsContent value='glycemie' className="grid grid-cols-1 gap-6 md:grid-cols-3">
-                                
-                                    <div className='col-span-2'>
-                                        <GlycemiaLineChart />
-                                    </div>
-                                    <div className="space-y-6">
-                                        <Button
-                                            className='w-full'
-                                            onClick={() => {
-                                                navigate("/user/diabete");
-                                            }}
-                                        >
-                                            Enregistrer sa glycémie
-                                        </Button>
-                                        <DiabeteAlertCard />
-                                    </div>
-                               
-                            </TabsContent>
-                            <TabsContent value='hypertension'>
-                                <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-                                    <div className='col-span-2'>
-                                        <HypertensionBarChart />
-                                    </div>
-                                    <div className='flex flex-col h-full gap-4'>
-                                        <Button
-                                            className='w-full'
-                                            onClick={() => {
-                                                navigate("/user/hypertension");
-                                            }}
-                                        >
-                                            Enregistrer sa tension
-                                        </Button>
-                                        <HyperTensionAlertCard />
-                                    </div>
-                                </div>
-                            </TabsContent>
-                            <TabsContent value='rappel'>
-                                <div className='w-full'>
-                                    <ReminderCalendar />
-                                </div>
-                            </TabsContent>
-                        </Tabs>
-                    </main>
-
+            <main className="p-6 space-y-6">
+                <Tabs defaultValue='glycemie'>
+                    <div className="flex justify-center">
+                        <TabsList className="inline-flex justify-center space-x-4">
+                            <TabsTrigger value='glycemie'>Diabète</TabsTrigger>
+                            <TabsTrigger value='hypertension'>Hypertension</TabsTrigger>
+                            <TabsTrigger value='rappel'>Rappels</TabsTrigger>
+                        </TabsList>
+                    </div>
+                    <TabsContent value='glycemie' className="grid grid-cols-1 gap-6 md:grid-cols-3">
+                        
+                            <div className='col-span-2'>
+                                <GlycemiaLineChart />
+                            </div>
+                            <div className="space-y-6">
+                                <Button
+                                    className='w-full'
+                                    onClick={() => {
+                                        navigate("/user/diabete");
+                                    }}
+                                >
+                                    Enregistrer sa glycémie
+                                </Button>
+                                <DiabeteAlertCard />
+                            </div>
+                        
+                    </TabsContent>
+                    <TabsContent value='hypertension'>
+                        <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+                            <div className='col-span-2'>
+                                <HypertensionBarChart />
+                            </div>
+                            <div className='flex flex-col h-full gap-4'>
+                                <Button
+                                    className='w-full'
+                                    onClick={() => {
+                                        navigate("/user/hypertension");
+                                    }}
+                                >
+                                    Enregistrer sa tension
+                                </Button>
+                                <HyperTensionAlertCard />
+                            </div>
+                        </div>
+                    </TabsContent>
+                    <TabsContent value='rappel'>
+                        <div className='w-full'>
+                            <ReminderCalendar />
+                        </div>
+                    </TabsContent>
+                </Tabs>
+            </main>
         </>
     );
 };
