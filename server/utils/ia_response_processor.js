ebe9591f37ee89994d7a3ebcc8eae244c24f6a4c
walid <idirwalidhakim31@gmail.com>
2024-06-28 10:19:34 +0200

    adding records to db while submitting SMS for diabetes

--- server/utils/ia_response_processor.js
+++ server/utils/ia_response_processor.js
@@ -1,16 +1,19 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const {Message, Alert} = require('../models');
+const {Message, Alert, Diabete , Hypertension} = require('../models');
 
 const {sendMessageToUser} = require('../utils/sms_manager');
-const {getGlucoseThresholds,generateHighGlucoseLevelWarningMessage} = require('../utils/diabete_utils');
+const {generateHighGlucoseLevelWarningMessage} = require('../utils/diabete_utils');
+
+const {evaluerTension} = require('../utils/tension_utils');
 
 
 
 
 async function processIaResponse(iaResponse , userProfile , diseaseAndMesureFromMessage ) 
-{
+{   
+    let finalContent = "";
     switch (diseaseAndMesureFromMessage.maladie) {
         case 'Diabète':
         
@@ -21,8 +24,8 @@ async function processIaResponse(iaResponse , userProfile , diseaseAndMesureFrom
             }
             const warningMessage = generateHighGlucoseLevelWarningMessage(userInfos , diseaseAndMesureFromMessage.valeur/100);
 
-            const finalContent = iaResponse + "\n\n" + warningMessage;
-            sendMessageToUser(userProfile.phone, );
+            finalContent = iaResponse + "\n\n" + warningMessage;
+            sendMessageToUser(userProfile.phone,finalContent );
 
             //create a new message and link it to the sender
             //console.log("send sms to user with id", userProfile);
@@ -47,10 +50,57 @@ async function processIaResponse(iaResponse , userProfile , diseaseAndMesureFrom
                     });
                 }
             
+            // add glucose mesure to db (check later if you overwrite it the existing one or simply add it)
+            const newGlucoseEntry = await Diabete.create({
+                userID: userProfile.id,
+                date: new Date(),
+                glycemie : diseaseAndMesureFromMessage.valeur,
+                isAjeun : diseaseAndMesureFromMessage.aJeun
+            });
+
             
             break;
         case 'Hypertension':
-            sendMessageToUser("+33766837685", iaResponse);
+            //checker si la tension est inquétente 
+            
+            const tensionALertObject = evaluerTension(diseaseAndMesureFromMessage);
+
+            const isTensionAlert = tensionALertObject.niveauAlerte != 0;
+            finalContent = iaResponse + "\n" + isTensionAlert ? tensionALertObject.message : "";
+
+            //enregistrer le message en base 
+            const newTensionSMS = await Message.create({
+                userID : userProfile.id ,
+                content : finalContent,
+                date : new Date(),
+                messageType : isTensionAlert ? "info" : "warning"
+            }); 
+                
+            
+            // envoyer une alerte au cas ou 
+
+            if( isTensionAlert)
+                {
+                    console.log("send sms to user with this id for tension request ", userProfile.id);
+                    const newAlert = await Alert.create({
+                        userID: userProfile.id,
+                        date: new Date(),
+                        message: tensionALertObject.message,
+                        maladie: 'Hypertension',
+                    });
+                }
+
+            
+            // add glucose mesure to db (check later if you overwrite it the existing one or simply add it)
+            const [systolic, diastolic] = diseaseAndMesureFromMessage.valeur.split('/').map(Number);
+            const newTensionEntry = await Hypertension.create({
+                userID: userProfile.id,
+                date: new Date(),
+                systolic : systolic,
+                diastolic : diastolic
+            });
+
+            sendMessageToUser(userProfile.phone,finalContent );
             break;
 
         case 'Hypothyroïdie':
